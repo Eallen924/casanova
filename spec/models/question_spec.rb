109286169602c7e7964cb8f180caa8c07a9778a6
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-07 15:41:12 -0500

    implemented failing question model tests

---
+++ spec/models/question_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe Question do
+  context 'testing associations' do
+    it { should belong_to(:user) }
+    it { should have_many(:answers) }
+    it { should have_many(:comments) }
+  end
+
+  context 'testing attr_accessible' do
+    it { should allow_mass_assignment_of(:user_id) }
+    it { should allow_mass_assignment_of(:title) }
+    it { should allow_mass_assignment_of(:body) }
+  end
+
+  context 'testing validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:body) }
+  end
+end
