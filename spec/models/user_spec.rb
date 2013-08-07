4cfc127862d7dd0047ef23fbfa616c67af7555ee
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-07 15:53:25 -0500

    added user validation tests for password and password digest

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,15 +4,6 @@ describe User do
   it { should validate_presence_of(:username)              }  
   it { should validate_presence_of(:email)                 }  
   it { should validate_presence_of(:password)              }  
-  it { should validate_presence_of(:password_confirmation) }  
+  it { should validate_presence_of(:password_confirmation) }
+  it { should validate_presence_of(:password_digest)       }  
 end
-
-
-
-
-# context "given valid parameters" do
-#     it "user should be created" do
-#       User.new(username: "pri1229", email: "pri1229@gmail.com" password: "password", password_confirmation: "password_confirmation")
-#       expect(User.all.count).to eq
-#     end
-#   end
