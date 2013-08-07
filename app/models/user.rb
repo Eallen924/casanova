b4b82cdf89e72921b364a31ae3852141a8c6d8e7
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-07 18:03:33 -0500

    merged the user model/migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :questions
+
   validates :username, :presence => true
   validates :email,    :presence => true
   validates :password, :presence => true
