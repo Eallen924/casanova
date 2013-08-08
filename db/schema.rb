125a88b863535c753b348b1874838ba19c89b195
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-08 13:05:15 -0500

    questions implemented, no tests implemented

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130808012222) do
+ActiveRecord::Schema.define(:version => 20130808162208) do
+
+  create_table "answers", :force => true do |t|
+    t.text     "body"
+    t.integer  "user_id"
+    t.integer  "question_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "comments", :force => true do |t|
     t.integer  "user_id"
