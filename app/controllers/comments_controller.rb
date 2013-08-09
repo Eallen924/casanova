94935b395381938455a27c5d5a72f5bbaae8d994
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-08 19:05:52 -0500

    removed instance variables for consistency and refactored redirect routes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,14 @@
 class CommentsController < ApplicationController
   def create
-    @question = Question.find(params[:question_id])
-    @comment = current_user.comments.build(text: params[:comment][:text])
-    @comment.commentable = @question
+    question = Question.find(params[:question_id])
+    comment = current_user.comments.build(params[:comment])
+    comment.commentable = question
 
-    if @comment.save
-      redirect_to question_path(@question)
+    if comment.save
+      redirect_to question
     else
-      flash[:errors] =  @comment.errors.full_messages
-      redirect_to question_path(@question)
+      flash[:errors] =  comment.errors.full_messages
+      redirect_to question
     end
   end
 end
